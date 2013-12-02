9a9dd4adb29a1662f864dc9af78ac14d61cb352c
Alejandro Vera <avera@conwet.com>
2013-12-02 10:40:02 +0000

    Eliminados los input point de servicio WFS (para obtener servicios enviados desde el catálogo de servicios) en el nomenclátor y el geonames. Actualizados los friendCOde de los output endpoint de todos los configuradores. Capacidad de enviar toda la información que se quiera (según se indique en la configuración) para ser mostrada en el mapa (bocadillos).

--- tests/spec/tests.js
+++ tests/spec/tests.js
@@ -2,19 +2,36 @@ var MashupPlatform = new MockMP.MockMP();
 
 describe( "GeoRSS", function () {  
     
+    var changeServiceCall;
+    var args;
+    var ajaxCall;
+    var options;
+    var successHandler;
+    
     beforeEach(function () {
+        changeServiceCall = MashupPlatform.wiring.registerCallback;
+        args = inputCall.mostRecentCall.args;
+        ajaxCall = MashupPlatform.http.makeRequest;
+        options = ajaxCall.mostRecentCall.args[1];
+        successHandler = spyOn(options, "onSuccess");
+        successHandler.andCallThrough();
     });
 
     afterEach(function () {
     });
 
-    describe("Initial status:", function () {
+    describe("Initial state:", function () {
         it("para que aparezca", function(){
            expect(1).toEqual(1); 
         });
     });
     
     describe("Inputs:", function () {
+        
+        it("add new GeoRSS service.", function () {
+            expect(2).toEqual(1); 
+        });
+        
     });
 
     describe("Outputs:", function () {
